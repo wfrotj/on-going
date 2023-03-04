451688be34e6b1e45309a0213a95b82bfd4facfc
wfortj <trebrodrigo@gmail.com>
2023-03-04 16:05:57 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <title>Yunah</title>
+     <link rel="icon" href="images/yaz.jpg">
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Yunah's Gallery</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+               <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Events
+                         </a>
+                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="#">Birthday</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item" href="#">Christmas</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item" href="#">Summer</a>
+                    <li class="nav-item">
+                         <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                         <a class="nav-link" href="#">Contact</a>
+                    </li>
+
+                    </li>
+               </ul>
+          </div>
+     </nav>
+     <aside>
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+               <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+
+               </ol>
+               <div class="carousel-inner">
+                    <div class="carousel-item active">
+                         <img src="images/birthday3.jpg" class="d-block w-100" alt=" birthday">
+                         <div class="carousel-caption d-none d-md-block">
+                              <h3>Birthday</h3>
+                              <p class="date">November 14, 2022</p>
+                         </div>
+                         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                              data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                         </a>
+                         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
+                              data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                         </a>
+                    </div>
+                    <div class="carousel-item">
+                         <img src="images/summer1.jpg" class="d-block w-100" alt="summer">
+                         <div class="carousel-caption d-none d-md-block">
+                              <h3>Summer</h3>
+                              <p class="date">May 18, 2022</p>
+                         </div>
+                         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                              data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                         </a>
+                         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
+                              data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                         </a>
+                    </div>
+                    <div class="carousel-item">
+                         <img src="images/christmas6.jpg" class="d-block w-100" alt="christmas">
+                         <div class="carousel-caption d-none d-md-block">
+                              <h3>Christmas</h3>
+                              <p class="date">December 25, 2022</p>
+                         </div>
+                         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                              data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                         </a>
+                         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
+                              data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                         </a>
+     </aside>
+
+
+
+
+
+
+
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
